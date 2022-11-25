cf3868fe8c997023bc553f956b0f301774df213c
Aldry Albuquerque <albuquerquealdry@gmail.com>
2022-11-24 21:37:05 -0300

    add. asd

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask import request
-app = Flask(__name__)
 
+app = Flask(__name__)
 @app.route('/action')
 def hello_world():
     action = request.args.get('action')
     return (f"{action}")
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
